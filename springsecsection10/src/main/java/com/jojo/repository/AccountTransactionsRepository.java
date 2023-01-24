5e142d8f919c3ed76af1b9de8eab4471df58a117
João Vitor <joaovlopes000@gmail.com>
2023-01-24 20:30:18 -0300

    section 10

---
+++ springsecsection10/src/main/java/com/jojo/repository/AccountTransactionsRepository.java
@@ -0,0 +1,15 @@
+package com.jojo.repository;
+
+import java.util.List;
+
+import com.jojo.model.AccountTransactions;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AccountTransactionsRepository extends CrudRepository<AccountTransactions, Long> {
+	
+	List<AccountTransactions> findByCustomerIdOrderByTransactionDtDesc(int customerId);
+
+}
---
+++ springsecsection10/src/main/java/com/jojo/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package com.jojo.repository;
+
+import com.jojo.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+	
+	Accounts findByCustomerId(int customerId);
+
+}
